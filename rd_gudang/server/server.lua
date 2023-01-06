efafb742ae03b851615b67858193960f5f24c6df
Dndmee <73814604+Dndmee@users.noreply.github.com>
2023-01-06 20:35:03 +0700

    Update server.lua

--- rd_gudang/server/server.lua
+++ rd_gudang/server/server.lua
@@ -5,8 +5,8 @@ local Delay = false
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 ESX.RegisterServerCallback('rd_gudang:checkLocker', function(source, cb, lockerId)
-	local braveid = source
-	local xPlayer = ESX.GetPlayerFromId(braveid)
+	local rdid = source
+	local xPlayer = ESX.GetPlayerFromId(rdid)
 	MySQL.Async.fetchAll('SELECT * FROM rd_gudang WHERE lockerName = @lockerId AND identifier = @identifier', { ['@lockerId'] = lockerId, ['@identifier'] = xPlayer.identifier }, function(result) 
 		if result[1] ~= nil then
 			cb(true)
@@ -19,34 +19,34 @@ end)
 
 RegisterServerEvent('rd_gudang:startRentingLocker')
 AddEventHandler('rd_gudang:startRentingLocker', function(lockerId, lockerName) 
-	local braveid = source
-	local xPlayer = ESX.GetPlayerFromId(braveid)
+	local rdid = source
+	local xPlayer = ESX.GetPlayerFromId(rdid)
 			if xPlayer.getMoney() >= RD.InitialRentPrice then
 				MySQL.Async.execute('INSERT INTO rd_gudang (identifier, lockerName) VALUES (@identifier, @lockerId)', {
 					['@identifier'] = xPlayer.identifier,
 					['@lockerId'] = lockerId
 				})
 				xPlayer.removeMoney(RD.InitialRentPrice)
-				TriggerClientEvent('mythic_notify:client:SendAlert', braveid, { type = 'success', text = "Kamu mulai menyewa " ..lockerName.. ". Kamu akan dikenakan biaya Rp."..RD.DailyRentPrice.." harian (IDR)", length = 5000 })
+				TriggerClientEvent('mythic_notify:client:SendAlert', rdid, { type = 'success', text = "Kamu mulai menyewa " ..lockerName.. ". Kamu akan dikenakan biaya Rp."..RD.DailyRentPrice.." harian (IDR)", length = 5000 })
 			else
-				TriggerClientEvent('mythic_notify:client:SendAlert', braveid, { type = 'error', text = "Kamu tidak memiliki uang untuk membayar biaya penyewaan pertama.", length = 5000 })
+				TriggerClientEvent('mythic_notify:client:SendAlert', rdid, { type = 'error', text = "Kamu tidak memiliki uang untuk membayar biaya penyewaan pertama.", length = 5000 })
 			end
 	end)
 
 
 RegisterServerEvent('rd_gudang:stopRentingLocker')
 AddEventHandler('rd_gudang:stopRentingLocker', function(lockerId, lockerName) 
-	local braveid = source
-	local xPlayer = ESX.GetPlayerFromId(braveid)
+	local rdid = source
+	local xPlayer = ESX.GetPlayerFromId(rdid)
 	MySQL.Async.fetchAll('SELECT * FROM rd_gudang WHERE lockerName = @lockerId AND identifier = @identifier', { ['@lockerId'] = lockerId, ['@identifier'] = xPlayer.identifier }, function(result)
 		if result[1] ~= nil then
 			MySQL.Async.execute('DELETE from rd_gudang WHERE lockerName = @lockerId AND identifier = @identifier', {
 				['@lockerId'] = lockerId,
 				['@identifier'] = xPlayer.identifier
 			})
-			TriggerClientEvent('mythic_notify:client:SendAlert', braveid, { type = 'inform', text = "Membatalkan penyewaan gudang.", length = 5000 })
+			TriggerClientEvent('mythic_notify:client:SendAlert', rdid, { type = 'inform', text = "Membatalkan penyewaan gudang.", length = 5000 })
 		else
-			TriggerClientEvent('mythic_notify:client:SendAlert', braveid, { type = 'error', text = "Kamu tidak memiliki gudang ini.", length = 5000 })
+			TriggerClientEvent('mythic_notify:client:SendAlert', rdid, { type = 'error', text = "Kamu tidak memiliki gudang ini.", length = 5000 })
 		end
 	end)
 end)
