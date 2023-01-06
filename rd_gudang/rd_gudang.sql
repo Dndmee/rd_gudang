cf37e24397a4081257a185d6b7f118c58ce61a66
Dndmee <73814604+Dndmee@users.noreply.github.com>
2023-01-06 20:36:34 +0700

    Update rd_gudang.sql

--- rd_gudang/rd_gudang.sql
+++ rd_gudang/rd_gudang.sql
@@ -1,34 +1,7 @@
--- --------------------------------------------------------
--- Host:                         202.83.121.205
--- Server version:               10.8.5-MariaDB - MariaDB Server
--- Server OS:                    Linux
--- HeidiSQL Version:             12.2.0.6576
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- Dumping database structure for secret_maindb
 CREATE DATABASE IF NOT EXISTS `` /*!40100 DEFAULT CHARACTER SET utf8mb4 */; -- ini di isi sama nama database mu ya
 USE ``;
 
--- Dumping structure for table secret_maindb.brave_locker
 CREATE TABLE IF NOT EXISTS `rd_gudang` (
   `identifier` varchar(50) NOT NULL,
   `lockerName` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
